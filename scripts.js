015825690230b3af55e3a8b033fac0830aa731ae
Shashwat Anand <shashwat.anand@gmail.com>
2016-02-11 23:15:10 +0530

    Update

--- scripts.js
+++ scripts.js
@@ -28,3 +28,54 @@ var student2 = new Student({
 });
 
 var students = new Students([student1, student2]);
+//var students = new Students();
+
+// View for one student
+
+var StudentView = Backbone.View.extend({
+	model: new Student(),
+	tagName: 'tr',
+	initailize: function() {
+		this.template($('students-list-template').html());
+	},
+	render: function() {
+		this.$el.html(this.template(this.model.toJSON()));
+		return this;
+	}
+});
+
+// View for student collection
+var StudentsView = Backbone.View.extend({
+	model: students,
+	el: $('.students-list'),
+	initailize: function() {
+		this.model.on('add', this.render, this);
+	},
+
+	render: function() {
+		var self = this;
+		this.$el.html('');
+		_.each(this.model.toArray(), function(student){
+			self.$el.append((new StudentView({model: student})).render().$el);
+		});
+		return this;
+	}
+});
+
+var studentsView = new StudentsView();
+
+$(document).ready(function(){
+	$('.add-student').on('click', function() {
+		var student = new Student({
+			firstname: $('.firstname-input').val(),
+			lastname: $('.lastname-input').val(),
+			id: $('.id-input').val(),
+		});
+		console.log(student.toJSON());
+		$('.firstname-input').val('');
+		$('.lastname-input').val('');
+		$('.id-input').val('');
+		students.add(student);
+		console.log(students.toJSON());
+	});
+});
